4335dd784833b496c5923e644e22efb408a579e5
slolee <slolee@naver.com>
2023-01-25 20:36:47 +0900

    Feat: 프로젝트 생성

---
+++ src/main/java/com/example/moonsunstudy/MoonsunStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.moonsunstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonsunStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonsunStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moonsunstudy/MoonsunStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moonsunstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonsunStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
